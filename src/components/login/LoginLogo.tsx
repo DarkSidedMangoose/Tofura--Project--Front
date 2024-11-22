45336b1cb0d01396f20bb79dfc672ea20841e74b
tofsona14 <topuria2074@gmail.com>
2024-11-22 15:52:02 +0400

    checking

--- src/components/login/LoginLogo.tsx
+++ src/components/login/LoginLogo.tsx
@@ -1,14 +1,15 @@
-import React from "react";
-import MainLogo from "../../assets/images/mainLogo.png";
-import MainLogoCenter from "../../assets/images/mainLogoCenter.png";
-import "./LoginLogo.css";
+import React from 'react'
+import MainLogo from '../../assets/images/mainLogo.png'
+import MainLogoCenter from '../../assets/images/mainLogoCenter.png'
+import './LoginLogo.css'
 const LoginLogo = () => {
+  //* check
   return (
     <div className="MainLogoDiv">
       <img className="MainLogo" src={MainLogo} alt="Logo"></img>
       <img className="MainMovedLogo" src={MainLogoCenter} alt="Logo"></img>
     </div>
-  );
-};
+  )
+}
 
-export default LoginLogo;
+export default LoginLogo
