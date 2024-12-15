13a6a7ece31cf0c8955d51df9a32385bfdeeaf54
tofsona14 <topuria2074@gmail.com>
2024-12-15 17:43:42 +0400

    add extra buttons

--- src/components/main/MainMain.tsx
+++ src/components/main/MainMain.tsx
@@ -6,7 +6,7 @@ export {}
 const MainMain: React.FC = () => {
   return (
     <div className=" w-90% h-100% bg-gray-800 z-10  flex justify-center items-center relative ">
-      <div className="w-98% h-95% flex  bg-white">
+      <div className="w-98% h-95% flex  bg-gray-500">
         <CommonBaseClickers />
       </div>
     </div>
