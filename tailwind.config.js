1edd50ba847f0f498dacca343b7e655d46ca9443
Tofura2074 <tokaoka12@gmail.com>
2024-12-17 17:09:46 +0400

    add tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      screens: {
+        xs: "480px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+        "3xl": "1920px",
+        "4xl": "2560px",
+      },
       inset: {
         "-320px": "-320px",
         "-220px": "-220px",
