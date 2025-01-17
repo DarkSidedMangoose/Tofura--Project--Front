17da8a18e16a62a174352f7f58123c63ba43d2bb
Tofura2074 <tokaoka12@gmail.com>
2025-01-17 21:57:57 +0400

    make our code more flexible readable and well-structured

--- src/components/main/MainSidebar.tsx
+++ src/components/main/MainSidebar.tsx
@@ -4,66 +4,111 @@ import Folder from "../../assets/images/main/folder.png";
 import Dashboard from "../../assets/images/main/dashboard.png";
 import MyProfile from "../../assets/images/main/myProfile.png";
 import "./mainAnimations.css";
+
+//interfaces of props
+
+interface sideState {
+  identifier: string;
+  state: boolean;
+}
+
+interface NavItemProps {
+  icon: string;
+  alt: string;
+  isActive: boolean;
+  onClick: () => void;
+}
+
+interface AdditionalInfoOfFolderProps {
+  isActive: boolean;
+}
+/* end of interfaces */
+
+//subComponents of main component
+const NavItem: React.FC<NavItemProps> = ({ icon, alt, isActive, onClick }) => {
+  return (
+    <div
+      onClick={onClick}
+      className={`w-full h-1/5 flex justify-center items-center  rounded-[100%] hover:opacity-70 cursor-pointer ${
+        isActive && "bg-blue-900"
+      }`}
+    >
+      <img className="w-[40%] h-[40%]" src={icon} alt={alt}></img>
+    </div>
+  );
+};
+
+const AdditionalInfoOfFolder: React.FC<AdditionalInfoOfFolderProps> = ({
+  isActive,
+}) => {
+  return (
+    <div
+      className={`z-0 ${
+        isActive ? "sidebarFolderInfoShown-div" : "sidebarFolderInfoClose-div"
+      } w-[77%] h-full bg-white shadow-bottom-right rounded-br-2xl `}
+    ></div>
+  );
+};
+/* end of subComponents */
+
+//main component
 const MainSidebar: React.FC = () => {
-  const [choosedSidebar, setChoosedSidebar] = useState<string>("folder"); // to identify which icon is choosed and what is state of our main
-  const [folderInfoAnimToBack, setFolderInfoAnimToBack] =
-    useState<boolean>(false); // additional state to make move to back animation of folder additional info
+  const [sidebarStates, setSidebarStates] = useState<sideState>({
+    identifier: "folder",
+    state: true,
+  });
+
+  const handleSidebarClick = (sidebar: string) => {
+    if (sidebar === "folder") {
+      setSidebarStates({ identifier: sidebar, state: true });
+    } else if (sidebar !== "folder" && sidebarStates.identifier === "folder") {
+      setSidebarStates({ ...sidebarStates, state: false });
+
+      setTimeout(() => {
+        setSidebarStates({ ...sidebarStates, identifier: sidebar });
+      }, 500);
+    } else {
+      setSidebarStates({ ...sidebarStates, identifier: sidebar });
+    }
+  };
+
   return (
     <div
       className={`${
-        choosedSidebar === "folder"
+        sidebarStates.identifier === "folder"
           ? "w-[20%] min-w-[350px]"
           : "w-[4.6%] min-w-[80.5px]"
       } flex  min-h-[700px] h-full bg-blue `}
     >
       <aside
         className={`z-10 ${
-          choosedSidebar === "folder" ? "w-[23%]" : "w-full"
+          sidebarStates.identifier === "folder" ? "w-[23%]" : "w-full"
         } h-full  bg-sidebarChoose flex flex-col items-center shadow-bottom-right`}
       >
         <img src={MainLogo} className="w-[70%]   mt-[30%]" alt="logo"></img>
         <nav className="w-[60%] h-[30%]  mt-[40%]  ">
-          <div
-            onClick={() => setChoosedSidebar("folder")}
-            className={`w-full h-1/5 flex justify-center items-center  rounded-[100%] hover:opacity-70 cursor-pointer ${
-              choosedSidebar === "folder" && "bg-blue-900"
-            }`}
-          >
-            <img className="w-[40%] h-[40%]" src={Folder} alt="folder"></img>
-          </div>
-          <div
-            onClick={() => {
-              setTimeout(() => {
-                setChoosedSidebar("dashboard");
-                setFolderInfoAnimToBack(false);
-              }, 500);
-              setFolderInfoAnimToBack(true);
-            }}
-            className="w-full h-1/5 flex justify-center items-center  rounded-[100%] hover:opacity-70 cursor-pointer"
-          >
-            <img
-              className="w-[40%] h-[40%]"
-              src={Dashboard}
-              alt="dashboard"
-            ></img>
-          </div>
-          <div className="w-full h-1/5 flex justify-center items-center  rounded-[100%] hover:opacity-70 cursor-pointer">
-            <img
-              className="w-[40%] h-[40%]"
-              src={MyProfile}
-              alt="myProfile"
-            ></img>
-          </div>
+          <NavItem
+            icon={Folder}
+            alt="folder"
+            isActive={sidebarStates.identifier === "folder"}
+            onClick={() => handleSidebarClick("folder")}
+          />
+          <NavItem
+            icon={Dashboard}
+            alt="dashboard"
+            isActive={sidebarStates.identifier === "dashboard"}
+            onClick={() => handleSidebarClick("dashboard")}
+          />
+          <NavItem
+            icon={MyProfile}
+            alt="profile"
+            isActive={sidebarStates.identifier === "profile"}
+            onClick={() => handleSidebarClick("profile")}
+          />
         </nav>
       </aside>
-      {choosedSidebar === "folder" && (
-        <div
-          className={`z-0 ${
-            folderInfoAnimToBack === false
-              ? "sidebarFolderInfoShown-div"
-              : "sidebarFolderInfoClose-div"
-          } w-[77%] h-full bg-white shadow-bottom-right rounded-br-2xl `}
-        ></div>
+      {sidebarStates.identifier === "folder" && (
+        <AdditionalInfoOfFolder isActive={sidebarStates.state} />
       )}
     </div>
   );
