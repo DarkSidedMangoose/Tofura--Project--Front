c38429bc835aaae7ff98547f44ff75377b954993
Tofura2074 <tokaoka12@gmail.com>
2025-01-18 23:06:38 +0400

    everything is excellent

--- src/components/main/MainSidebar.tsx
+++ src/components/main/MainSidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, memo } from "react";
 import MainLogo from "../../assets/images/main/fullLogo.png";
 import Folder from "../../assets/images/main/folder.png";
 import Dashboard from "../../assets/images/main/dashboard.png";
@@ -19,34 +19,111 @@ interface NavItemProps {
   onClick: () => void;
 }
 
-interface AdditionalInfoOfFolderProps {
+interface AdditionalInfoOfBaseProps {
   isActive: boolean;
+}
+interface AdditionalInfoOfBaseStates {
+  baseInfoP: string[];
+  baseInfoChoose: string;
+  baseInfoNdP: string[];
 }
 /* end of interfaces */
 
 //subComponents of main component
-const NavItem: React.FC<NavItemProps> = ({ icon, alt, isActive, onClick }) => {
-  return (
-    <div
-      onClick={onClick}
-      className={`w-full h-1/5 flex justify-center items-center  rounded-[100%] hover:opacity-70 cursor-pointer ${
-        isActive && "bg-blue-900"
-      }`}
-    >
-      <img className="w-[40%] h-[40%]" src={icon} alt={alt}></img>
-    </div>
-  );
-};
 
-const AdditionalInfoOfFolder: React.FC<AdditionalInfoOfFolderProps> = ({
+interface NavItemProps {
+  icon: string;
+  alt: string;
+  isActive: boolean;
+  onClick: () => void;
+}
+
+const NavItem: React.FC<NavItemProps> = memo(
+  ({ icon, alt, isActive, onClick }) => {
+    return (
+      <div
+        onClick={onClick}
+        className={`w-full h-1/5 flex justify-center items-center rounded-[100%] hover:opacity-70 cursor-pointer ${
+          isActive ? "bg-blue-900" : ""
+        }`}
+      >
+        <div className="w-[40%] aspect-w-1 aspect-h-1">
+          <img className="object-contain" src={icon} alt={alt} />
+        </div>
+      </div>
+    );
+  }
+);
+
+const AdditionalInfoOfBase: React.FC<AdditionalInfoOfBaseProps> = ({
   isActive,
 }) => {
+  const [baseState, setBaseState] = useState<AdditionalInfoOfBaseStates>({
+    baseInfoP: [
+      "ობიექტების რეესტრი",
+      "ინსპექტირების ობიექტები",
+      "შემოწმებული ობიექტების რეესტრი",
+    ],
+    baseInfoChoose: "",
+    baseInfoNdP: [
+      "ახალი ობიექტები",
+      "შემოწმებული ობიექტები",
+      "წაშლილი ობიექტები",
+    ],
+  });
   return (
     <div
       className={`z-0 ${
         isActive ? "sidebarFolderInfoShown-div" : "sidebarFolderInfoClose-div"
-      } w-[77%] h-full bg-white shadow-bottom-right rounded-br-2xl `}
-    ></div>
+      } w-[77%] h-full bg-white shadow-bottom-right rounded-br-2xl flex justify-center items-center  `}
+    >
+      <div className=" w-90% h-[90%] flex flex-col space-y-8   ">
+        <div className="space-y-24">
+          <section className="flex flex-col space-y-4  ">
+            <h1 className="flex justify-center text-blue-950 font-bold">
+              მონაცემთა ბაზები
+            </h1>
+            {baseState.baseInfoP.map((info, index) => (
+              <p
+                className={`${
+                  baseState.baseInfoChoose === info
+                    ? "font-bold sidebarAdditionalInfoChoose "
+                    : " sidebarAdditionalInfoChooseOff"
+                } text-[0.9rem] cursor-pointer text-blue-950 `}
+                key={index}
+                onClick={
+                  () =>
+                    setBaseState((prev) => ({ ...prev, baseInfoChoose: info })) // to identify which has choose
+                }
+              >
+                {info}
+              </p>
+            ))}
+          </section>
+          <section className="flex flex-col space-y-4  ">
+            <h1 className="flex justify-center text-blue-950 font-bold">
+              დამატებითი ბაზები
+            </h1>
+            {baseState.baseInfoNdP.map((info, index) => (
+              <p
+                className={`${
+                  baseState.baseInfoChoose === info
+                    ? "font-bold sidebarAdditionalInfoChoose "
+                    : " sidebarAdditionalInfoChooseOff"
+                } text-[0.9rem] cursor-pointer text-blue-950 `}
+                key={index}
+                onClick={
+                  () =>
+                    setBaseState((prev) => ({ ...prev, baseInfoChoose: info })) // to identify which has choose
+                }
+              >
+                {info}
+              </p>
+            ))}
+          </section>
+        </div>
+      </div>
+    </div>
   );
 };
 /* end of subComponents */
@@ -108,7 +185,7 @@ const MainSidebar: React.FC = () => {
         </nav>
       </aside>
       {sidebarStates.identifier === "folder" && (
-        <AdditionalInfoOfFolder isActive={sidebarStates.state} />
+        <AdditionalInfoOfBase isActive={sidebarStates.state} />
       )}
     </div>
   );
--- src/components/main/mainFooter.tsx
+++ src/components/main/mainFooter.tsx
@@ -6,32 +6,32 @@ import email from "../../assets/images/main/email.png";
 
 const MainFooter: React.FC = () => {
   return (
-    <footer className=" fixed  bottom-0  w-full h-[5%] min-h-[50px] z-10 flex justify-center footerAnimated-div ">
-      <div className="bg-white border-2 flex items-center justify-between   w-[98%] shadow-top  h-full rounded-lg">
-        <div className="ml-[2%]  flex w-[18%] narrow:w-[50%] h-50% items-center ">
-          <img
-            className=" w-9% h-[100%] rotate-[60deg]  "
-            alt="navigationIcon"
-            src={iconNavigation}
-          ></img>
-          <p className="ml-2 text-[0.9vw] narrow:text-[15px] w-100% flex-grow  text-footerText font-bold  h-auto    ">
+    <footer className="fixed bottom-0 w-full h-12 z-10 flex justify-center footerAnimated-div">
+      <div className="bg-white border-2 flex items-center justify-between w-11/12 shadow-top h-full rounded-lg">
+        <section className="ml-4 flex items-center space-x-2">
+          <div className=" w-6  aspect-w-1 aspect-h-1 transform rotate-[60deg]">
+            <img
+              className=" object-contain"
+              alt="navigationIcon"
+              src={iconNavigation}
+            />
+          </div>
+          <p className="text-sm font-bold text-footerText">
             შრომის ინსპექციის სამსახური
           </p>
-        </div>
-        <div className="mr-[2%] w-[30%] flex justify-between items-center gap-4 ">
-          <div className="flex w-50% h-50% justify-center items-center">
-            <img className="w-[10%] h-[10%]" alt="phoneIcon" src={phone}></img>
-            <p className="ml-2 text-[0.9vw] narrow:text-[15px] w-100% flex-grow  text-footerText font-bold  h-auto    ">
-              0322-22-22-22
-            </p>
+        </section>
+        <section className="mr-4 flex space-x-8">
+          <div className="flex items-center space-x-2">
+            <img className="w-6 h-6" alt="phoneIcon" src={phone} />
+            <p className="text-sm font-bold text-footerText">0322-22-22-22</p>
           </div>
-          <div className="flex w-50%  h-50% justify-center items-center gap-4">
-            <img className="w-[10%] h-[10%] " alt="emailIcon" src={email}></img>
-            <p className="ml-2 text-[0.9vw] narrow:text-[15px] w-100% flex-grow  text-footerText font-bold  h-auto    ">
+          <div className="flex items-center space-x-2">
+            <img className="w-6 h-6" alt="emailIcon" src={email} />
+            <p className="text-sm font-bold text-footerText">
               workinspect.moh.gov.ge
             </p>
           </div>
-        </div>
+        </section>
       </div>
     </footer>
   );
