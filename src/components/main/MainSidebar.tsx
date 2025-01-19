e48f0c3eb8e7aa327c11652d122e9ea4945fb85c
Tofura2074 <tokaoka12@gmail.com>
2025-01-19 11:35:57 +0400

    fix names

--- src/components/main/MainSidebar.tsx
+++ src/components/main/MainSidebar.tsx
@@ -1,6 +1,6 @@
 import React, { useState, memo } from "react";
 import MainLogo from "../../assets/images/main/fullLogo.png";
-import Folder from "../../assets/images/main/folder.png";
+import Base from "../../assets/images/main/base.png";
 import Dashboard from "../../assets/images/main/dashboard.png";
 import MyProfile from "../../assets/images/main/myProfile.png";
 import "./mainAnimations.css";
@@ -74,7 +74,7 @@ const AdditionalInfoOfBase: React.FC<AdditionalInfoOfBaseProps> = ({
   return (
     <div
       className={`z-0 ${
-        isActive ? "sidebarFolderInfoShown-div" : "sidebarFolderInfoClose-div"
+        isActive ? "sidebarBaseInfoShown-div" : "sidebarBaseInfoClose-div"
       } w-[77%] h-full bg-white shadow-bottom-right rounded-br-2xl flex justify-center items-center  `}
     >
       <div className=" w-90% h-[90%] flex flex-col space-y-8   ">
@@ -131,14 +131,14 @@ const AdditionalInfoOfBase: React.FC<AdditionalInfoOfBaseProps> = ({
 //main component
 const MainSidebar: React.FC = () => {
   const [sidebarStates, setSidebarStates] = useState<sideState>({
-    identifier: "folder",
+    identifier: "base",
     state: true,
   });
 
   const handleSidebarClick = (sidebar: string) => {
-    if (sidebar === "folder") {
+    if (sidebar === "base") {
       setSidebarStates({ identifier: sidebar, state: true });
-    } else if (sidebar !== "folder" && sidebarStates.identifier === "folder") {
+    } else if (sidebar !== "base" && sidebarStates.identifier === "base") {
       setSidebarStates({ ...sidebarStates, state: false });
 
       setTimeout(() => {
@@ -152,23 +152,23 @@ const MainSidebar: React.FC = () => {
   return (
     <div
       className={`${
-        sidebarStates.identifier === "folder"
+        sidebarStates.identifier === "base"
           ? "w-[20%] min-w-[350px]"
           : "w-[4.6%] min-w-[80.5px]"
       } flex  min-h-[700px] h-full bg-blue `}
     >
       <aside
         className={`z-10 ${
-          sidebarStates.identifier === "folder" ? "w-[23%]" : "w-full"
+          sidebarStates.identifier === "base" ? "w-[23%]" : "w-full"
         } h-full  bg-sidebarChoose flex flex-col items-center shadow-bottom-right`}
       >
         <img src={MainLogo} className="w-[70%]   mt-[30%]" alt="logo"></img>
         <nav className="w-[60%] h-[30%]  mt-[40%]  ">
           <NavItem
-            icon={Folder}
-            alt="folder"
-            isActive={sidebarStates.identifier === "folder"}
-            onClick={() => handleSidebarClick("folder")}
+            icon={Base}
+            alt="Base"
+            isActive={sidebarStates.identifier === "base"}
+            onClick={() => handleSidebarClick("base")}
           />
           <NavItem
             icon={Dashboard}
@@ -184,7 +184,7 @@ const MainSidebar: React.FC = () => {
           />
         </nav>
       </aside>
-      {sidebarStates.identifier === "folder" && (
+      {sidebarStates.identifier === "base" && (
         <AdditionalInfoOfBase isActive={sidebarStates.state} />
       )}
     </div>
