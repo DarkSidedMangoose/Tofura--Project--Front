0f86e5951756bc8a39fc584f3aa234ca4905a497
Tofura2074 <tokaoka12@gmail.com>
2025-02-07 21:11:22 +0400

    change lots of things

--- src/components/main/subComponents/mainMain/mainDashboardSubComponents/subComponents/MapCountryWithRegions/CountryWithRegions.tsx
+++ src/components/main/subComponents/mainMain/mainDashboardSubComponents/subComponents/MapCountryWithRegions/CountryWithRegions.tsx
@@ -7,9 +7,9 @@ const CountryWithRegions: React.FC = () => {
   const [identifyRegion, setIdentifyRegion] = useState<number>(11);
   return (
     <div className="w-full h-full  rounded-2xl flex flex-col  items-start justify-between shadow-boxShadow object-contain ">
-      <main className="w-full h-60% flex justify-evenly items-center bg-loginBackground shadow-boxShadow rounded-2xl">
-        <div className="w-60% h-90% flex justify-center items-center bg-footerText rounded-2xl shadow-boxShadow">
-          <div className="w-95% h-90% rounded-2xl  shadow-boxShadow flex justify-center items-center  bg-white  ">
+      <main className="w-full h-60% flex justify-evenly items-center bg-loginBackground shadow-boxShadow  rounded-2xl">
+        <div className="w-60% h-90% flex justify-center items-center  rounded-2xl  ">
+          <div className="w-95% h-90% rounded-2xl  shadow-boxShadow flex justify-center items-center  bg-white border-2 border-sidebarChoose   ">
             <ComposableMap
               className="w-full h-full "
               projection="geoMercator"
@@ -55,12 +55,31 @@ const CountryWithRegionsAdditionalInfo: React.FC<
   CountryWithRegionsAdditionalInfoProps
 > = ({ identifyRegion }) => {
   return (
-    <div className=" h-90% w-[35%] shadow-boxShadow rounded-2xl">
-      <p className="font-bold text-sidebarChoose shadow-bottom bg-white rounded-tl-lg rounded-tr-lg text-m m-0 p-0 h-10% w-full flex justify-center items-center ">
+    <div className=" h-90% w-[35%] rounded-2xl flex items-center justify-center flex-col ">
+      {/* <p className="font-bold text-sidebarChoose shadow-bottom bg-white rounded-bl-lg rounded-br-lg text-m m-0 p-0 h-10% w-[95%] flex justify-center items-center ">
         {MapObjects[identifyRegion - 1].region}
-      </p>
-      <div className="w-full h-90% bg-footerText rounded-bl-lg rounded-br-lg flex justify-center items-center ">
-        <div className="w-[95%] h-[95%] bg-white rounded-2xl"></div>
+      </p> */}
+      <div className="w-[95%] h-[90%]  rounded-2xl shadow-boxShadow border-2 border-sidebarChoose bg-white">
+        <header className="w-full h-10% flex items-center justify-center  border-b-2 border-sidebarChoose rounded-tr-2xl rounded-tl-2xl">
+          <p className="text-sidebarChoose font-bold">
+            {MapObjects[identifyRegion - 1].region}
+          </p>
+        </header>
+        <body className="w-full h-90%  flex font-semibold bg-white text-sidebarChoose items-center justify-center rounded-bl-2xl rounded-br-2xl">
+          <section className="w-[95%] h-[95%] flex flex-col gap-[5%] ">
+            <p>
+              რეგიონში გამოვლენილი დარღვევა:
+              {MapObjects[identifyRegion - 1].accidents}
+            </p>
+            <p>
+              გამოსწორებული დარღვევა: {MapObjects[identifyRegion - 1].corrected}
+            </p>
+            <p>
+              გამოვლენილი უბედური შემთხვევები:{" "}
+              {MapObjects[identifyRegion - 1].violation}
+            </p>
+          </section>
+        </body>
       </div>
     </div>
   );
